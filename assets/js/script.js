01165fc4ff646a55527a7708a81f31b3154377b3
Yan Romano <103431150+yanvrom@users.noreply.github.com>
2022-12-05 19:29:12 -0300

    msg alerta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event){
+    dinamica = document.getElementById('dinamica')
+    hidrostatica = document.getElementById('hidrostatica')
+    hidrodinamica = document.getElementById('hidrodinamica')
+    gravitacao = document.getElementById('gravitacao')
+    estatica = document.getElementById('estatica')
+
+    dinamica.addEventListener('click', function(event){
+        img = dinamica.querySelector('img')
+        img.src='assets/img/dinamica_bloqueada.png'
+    })
     
+    hidrostatica.addEventListener('click', function(event){
+        img2 = hidrostatica.querySelector('img')
+        img2.src='assets/img/hidrostatica_bloqueada.png'
+    })
+    hidrodinamica.addEventListener('click', function(event){
+        img3 = hidrodinamica.querySelector('img')
+        img3.src='assets/img/hidrodinamica_bloqueada.png'
+    })
+    gravitacao.addEventListener('click', function(event){
+        img4 = gravitacao.querySelector('img')
+        img4.src='assets/img/gravitacao_bloqueada.png'
+    })
+    estatica.addEventListener('click', function(event){
+        img5 = estatica.querySelector('img')
+        img5.src='assets/img/estatica_bloqueada.png'
+    })
 })
 
 
